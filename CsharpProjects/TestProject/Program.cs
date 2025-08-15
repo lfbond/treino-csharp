7e88f8d07a1a9903fa41e852a2268904187b8c69
Luis Felipe Bond <luisfelipe.bond@gmail.com>
2025-08-15 18:59:29 -0300

    comentarios no codigo

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,3 +1,8 @@
-﻿Random dice = new Random();
+﻿// Cria uma nova instância da classe Random para gerar números aleatórios
+Random dice = new Random();
+
+// Gera um número aleatório entre 1 e 6 (inclusive), simulando o lançamento de um dado
 int roll = dice.Next(1, 7);
+
+// Exibe o valor sorteado no console
 Console.WriteLine(roll);
