bf01685fba4653408f353c3550a62565636a92d3
Luis Felipe Bond <luisfelipe.bond@gmail.com>
2025-08-18 16:38:47 -0300

    commit v3-Unidade03

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,74 +1,4 @@
-﻿/*Exercicio 01
-Random dice = new Random();
-int roll1 = dice.Next(1, 7); // Generates a random number between 1 and 6
-int roll2 = dice.Next(1, 7); // Generates another random number between 1 and 6
-int roll3 = dice.Next(1, 7); // Generates a third random number between 1 and 6
-
-int total = roll1 + roll2 + roll3; // Sums the three rolls
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}"); */
-
-
-/*//Exercicio 02
-Random dice = new Random();
-int roll1 = dice.Next(1, 7); // Generates a random number between 1 and 6
-int roll2 = dice.Next(1, 7); // Generates another random number between 1 and 6
-int roll3 = dice.Next(1, 7); // Generates a third random number between 1 and 6
-
-int total = roll1 + roll2 + roll3; // Sums the three rolls
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if (total >= 12)
-{
-    Console.WriteLine("You win!");
-}
-
-if (total <= 11)
-{
-    Console.WriteLine("Sorry, you lose!");
-} */
-
-/* 
-//Exercicio 03
-string message = "The quick brown fox jumps over the lazy dog.";
-bool result = message.Contains("dog");
-Console.WriteLine(result);
-
-if (message.Contains("fox"))
-{
-    Console.WriteLine("What does the fox say?");
-} */
-
-/*Exercicio 04
-Random dice = new Random();
-int roll1 = dice.Next(1, 7); // Generates a random number between 1 and 6
-int roll2 = dice.Next(1, 7); // Generates another random number between 1 and 6
-int roll3 = dice.Next(1, 7); // Generates a third random number between 1 and 6
-
-int total = roll1 + roll2 + roll3; // Sums the three rolls
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-{
-Console.WriteLine("You rolled doubles! +2 bonus to total");
-Console.WriteLine(total + 2);
-}
-else if((roll1 != roll2) || (roll2 != roll3) || (roll1 != roll3)) {
-    
-    if (total < 12)
-    {
-        Console.WriteLine("You win!");
-    }
-
-    if (total >= 12)
-    {
-        Console.WriteLine("Sorry, you lose!");
-    }
-    
-} */
-
-//Exercicio 05
+﻿/* //Exercicio 01
 Random dice = new Random();
 int roll1 = dice.Next(1, 7); // Generates a random number between 1 and 6
 int roll2 = dice.Next(1, 7); // Generates another random number between 1 and 6
@@ -89,13 +19,121 @@ if ((roll1 == roll2) && (roll2 == roll3))
     Console.WriteLine(total + 6);
 }
 
-if (total < 12)
+if (total < 15)
 {
     Console.WriteLine("Sorry, you lose!");
 }
 
-if (total >= 12)
+else
+{
+    Console.WriteLine("You win!");
+}
+ */
+
+/*  //Exercicio 02
+Random dice = new Random();
+int roll1 = dice.Next(1, 7); // Generates a random number between 1 and 6
+int roll2 = dice.Next(1, 7); // Generates another random number between 1 and 6
+int roll3 = dice.Next(1, 7); // Generates a third random number between 1 and 6
+
+int total = roll1 + roll2 + roll3; // Sums the three rolls
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        Console.WriteLine(total + 6);
+
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus to total");
+        Console.WriteLine(total + 2);
+    }
+}
+
+if (total >= 15)
 {
     Console.WriteLine("You win!");
 }
 
+else
+{
+    Console.WriteLine("Sorry, you lose!");
+} */
+
+/* //Exercicio 03
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        Console.WriteLine(total + 6);
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        Console.WriteLine(total + 2);
+
+    }
+
+    Console.WriteLine($"Your total including the bonus: {total}");
+}
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+} */
+
+//Exercicio 04
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
