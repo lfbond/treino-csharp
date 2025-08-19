e9bb6b2d3d3cc5b08a370de0783927862086e2b8
Luis Felipe Bond <luisfelipe.bond@gmail.com>
2025-08-19 14:21:05 -0300

    2 commit  da v4-Unidade-3

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -29,10 +29,45 @@ Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 
 Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process."); */
 
-//Exercicio 03
+/* //Exercicio 03
 
 string[] names = { "Rowena", "Robin", "Bao" };
 foreach (string name in names)
 {
     Console.WriteLine(name);
+} */
+
+/* //Exercicio 04
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory."); */
+
+/* //Exercicio 05 - TREINO
+string[] Ids = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+string name = "Bob";
+foreach (string ids in Ids)
+{
+    if (name.StartsWith("B"))
+    {
+        Console.WriteLine($"The name starts with {Ids}!");
+    }
+} */
+
+//Exercicio 05 - CORREÇÃO
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
 }
