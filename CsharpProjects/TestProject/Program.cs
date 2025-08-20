352faa5dca9fff5b752b94cc68d22a33a0c9ca9c
Luis Felipe Bond <luisfelipe.bond@gmail.com>
2025-08-20 14:29:44 -0300

    Commit do Projeto exemplo

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,178 +1,244 @@
-﻿//Exercicio 01
+﻿/* //Exercício pratico do Módulo 2 inteiro - INICIO
 
-//Testing a change to the message.
-//int widgetsSold = 7;
-//Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
-/* string firstName = "Bob";
-int widgetsPurchased = 7;
+//initialize variables - graded assigments
+int currentAssigments = 5;
 
-Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets."); */
+int sophia1 = 90;
+int sophia2 = 86;
+int sophia3 = 87;
+int sophia4 = 98;
+int sophia5 = 100;
 
-/* //Exercicio 02 - com Comentários fracos
-Random random = new Random();
-string[] orderIDs = new string[5];
-//Loop through eac blank orderID
-for (int i = 0; i < orderIDs.Length; i++)
+int andrew1 = 92;
+int andrew2 = 89;
+int andrew3 = 81;
+int andrew4 = 96;
+int andrew5 = 90;
+
+int emma1 = 90;
+int emma2 = 85;
+int emma3 = 87;
+int emma4 = 98;
+int emma5 = 68;
+
+int logan1 = 90;
+int logan2 = 95;
+int logan3 = 87;
+int logan4 = 88;
+int logan5 = 96;
+
+int sophiaSum = 0;
+int andrewSum = 0;
+int emmaSum = 0;
+int loganSum = 0;
+
+decimal sophiaScore;
+decimal andrewScore;
+decimal emmaScore;
+decimal loganScore;
+
+sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
+emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
+loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
+
+sophiaScore = (decimal)sophiaSum / currentAssigments;
+andrewScore = (decimal)andrewSum / currentAssigments;
+emmaScore = (decimal)emmaSum / currentAssigments;
+loganScore = (decimal)loganSum / currentAssigments;
+
+Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
+Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
+Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine(); */
+
+/* //Exercício pratico do Módulo 2 inteiro - CONTINUAÇÃO 1
+using System.Diagnostics;
+
+int currentAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 }; 
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+int[] studentScores = new int[10];
+
+// Write the Report Header to the console
+Console.WriteLine("Student\t\tGrade\n");
+
+foreach (string name in studentNames)
 {
-    //Get a random value that equates to ASCII letters A through E
-    int prefixValue = random.Next(65, 70);
-    //Covert the random value into a char, then a string
-    string prefix = Convert.ToChar(prefixValue).ToString();
-    //Create a random number, pad with zeroes
-    string suffix = random.Next(1, 1000).ToString("000");
-    //Combine the prefix and suffix together, then assign to current OrderID
-    orderIDs[i] = prefix + suffix;
-}
-//Print out each orderID
-foreach (var orderID in orderIDs)
-{
-    Console.WriteLine(orderID);
-} */
+    string currentStudent = name;
+    //Console.WriteLine($"{name}");
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
 
-/* //Exercicio 02 - sem Comentários
-Random random = new Random();
-string[] orderIDs = new string[5];
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
 
-for (int i = 0; i < orderIDs.Length; i++)
-{
-    int prefixValue = random.Next(65, 70);
-    string prefix = Convert.ToChar(prefixValue).ToString();
-    string suffix = random.Next(1, 1000).ToString("000");
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
 
-    orderIDs[i] = prefix + suffix;
-}
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
 
-foreach (var orderID in orderIDs)
-{
-    Console.WriteLine(orderID);
-} */
+    //int sophiaSum = 0;
+    //decimal sophiaScore;
 
-/* //Exercicio 02 - com Comentários de nível superior
+    // initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
 
+    // initialize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
 
- //  The following code creates five random OrderIDs
- //  to test the fraud detection process. OrderIDs
- //  consist of a letter from A to E, and a three
- //  digit number. Ex. A123.
-
-Random random = new Random();
-string[] orderIDs = new string[5];
-
-for (int i = 0; i < orderIDs.Length; i++)
-{
-    int prefixValue = random.Next(65, 70);
-    string prefix = Convert.ToChar(prefixValue).ToString();
-    string suffix = random.Next(1, 1000).ToString("000");
-
-    orderIDs[i] = prefix + suffix;
-}
-
-foreach (var orderID in orderIDs)
-{
-    Console.WriteLine(orderID);
-} */
-
-/* //Exercício 03 - Exemplo01
-Console
-.
-WriteLine
-(
-"Hello Example 1!"
-)
-;
-
-// Example 2:
-string firstWord="Hello";string lastWord="Example 2";Console.WriteLine(firstWord+" "+lastWord+"!"); */
-
-/* //Exercício 03 - Exemplo02
-Random dice = new Random();
-
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-{
-    if ((roll1 == roll2) && (roll2 == roll3))
+    foreach (int score in studentScores)
     {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        Console.WriteLine($"Total = {total + 6}");
+        //add the exam score to the sum
+        sumAssignmentScores += score;
     }
+    currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+}
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine(); */
+
+
+//Exercício pratico do Módulo 2 inteiro - CONTINUAÇÃO Final
+using System.Diagnostics;
+
+int examAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 }; 
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 }; 
+
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
+
+int[] studentScores = new int[10];
+
+string currentStudentLetterGrade = "";
+
+// Write the Report Header to the console
+Console.WriteLine("Student\t\tGrade\n");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+    //Console.WriteLine($"{name}");
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    else if (currentStudent == "Becky")
+        studentScores = beckyScores;
+
+    else if (currentStudent == "Chris")
+        studentScores = chrisScores;
+
+    else if (currentStudent == "Eric")
+        studentScores = ericScores;
+        
+    else if (currentStudent == "Gregor")
+        studentScores = gregorScores;
     else
+        continue;
+
+    //int sophiaSum = 0;
+    //decimal sophiaScore;
+
+    // initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
+
+    // initialize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+
+    // initialize/reset a counter for the number of assignment 
+    int gradedAssignments = 0;
+
+    // loop through the scores array and complete calculations for currentStudent
+    foreach (int score in studentScores)
     {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        Console.WriteLine($"Total = {total + 2}");
+        //add the exam score to the sum
+        //sumAssignmentScores += score;
+        // increment the assignment counter
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+            // add the exam score to the sum
+            sumAssignmentScores += score;
+
+        else
+            // add the extra credit points to the sum - bonus points equal to 10% of an exam score
+            sumAssignmentScores += score / 10;
     }
-} */
 
-/* //Exercício 04 - DESAFIO
+    currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
 
-string str = "The quick brown fox jumps over the lazy dog.";
-// convert the message into a char array
-char[] charMessage = str.ToCharArray();
-// Reverse the chars
-Array.Reverse(charMessage);
-int x = 0;
-// count the o's
-foreach (char i in charMessage) { if (i == 'o') { x++; } }
-// convert it back to a string
-string new_message = new String(charMessage);
-// print it out
-Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times."); */
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
 
 
-
-/* //Exercício 04 - DESAFIO-SOLUÇÃO-MINHA
-
-//The high-level purpose of this code is to reverse a 
-//string and count the number of times a specific character appears.
-
-string messageOld = "The quick brown fox jumps over the lazy dog.";
-char[] message = messageOld.ToCharArray();
-
-Array.Reverse(message);
-int letterCount = 0;
-
-foreach (char letter in message)
-{
-    if (letter == 'o')
-    {
-        letterCount++;
-    }
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 }
 
-string new_message = new String(message);
-
-Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {letterCount} times."); */
-
-
-
-//Exercício 04 - DESAFIO-SOLUÇÃO-REAL
-//   This code reverses a message, counts the number of times 
-//   a particular character appears, then prints the results
-//   to the console window.
-
-string originalMessage = "The quick brown fox jumps over the lazy dog.";
-
-char[] message = originalMessage.ToCharArray();
-Array.Reverse(message);
-
-int letterCount = 0;
-
-foreach (char letter in message)
-{
-    if (letter == 'o')
-    {
-        letterCount++;
-    }
-}
-
-string newMessage = new String(message);
-
-Console.WriteLine(newMessage);
-Console.WriteLine($"'o' appears {letterCount} times.");
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
